bc2d2ec18fd9aa4aa093d3007604c88a4aab3bb9
Marcosluengo04 <marcos.lgortiz@gmail.com>
2024-11-25 14:24:12 +0100

    a
    a

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private GameObject pies;
+    [SerializeField] private float radiopies;
+    [SerializeField] private LayerMask queEssuelo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb =GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,4 +20,40 @@ public class Player : MonoBehaviour
     {
         
     }
+    //private void FixedUpdate()
+    //{
+    //    rb.AddForce(new Vector2(hInput,0)* fuerzmovimiento)
+    //}
+
+    private bool EnSuelo() 
+    {
+        //"coll" representa el collider detectado
+        Collider coll = Physics2D.OverlapCircle(pies.position, radiopies, queEssuelo );
+
+        if (coll != null)
+        {
+            return true;
+
+
+
+        }
+        else 
+        { 
+        
+            return false;
+        
+        }
+        
+        
+
+
+    }
+    //  Codigo para pintar 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(pies.position , radiopies);
+    }
+
+
+
 }
